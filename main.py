6e9b8ec04b09fdc907dfa33f09f12cb9c4e4f099
kvsrkrishnan <kvsrkrishnan@gmail.com>
2023-09-19 16:31:26 -0700

    Update file

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 print("Read Book")
+
+# Read from File
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
+
+# Output Read data
 print(file_contents)
 print("Finished Reading 'Frankenstein'")
+
+# Get all words in a string
+words = file_contents.split()
+
+# Count of all words in the string
+print(len(words))
+
